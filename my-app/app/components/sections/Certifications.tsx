b3e5791ac777117e95f4760c2da46e6ce8ea4530
Berohlfs <90404673+Berohlfs@users.noreply.github.com>
2025-08-28 22:22:23 -0300

    add duolingo and progress bar

--- my-app/app/components/sections/Certifications.tsx
+++ my-app/app/components/sections/Certifications.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image"
 import Link from "next/link"
 // Icons
-import { ExternalLink, FileQuestionMark } from "lucide-react"
+import { ExternalLink } from "lucide-react"
 // Shadcn
 import { Button } from "@/components/ui/button"
 
@@ -10,8 +10,9 @@ export const Certifications = () => {
 
     const certificates = [
         { title: 'Meta Front-End Developer', description: 'Professional Certificate', link: 'https://www.coursera.org/account/accomplishments/professional-cert/certificate/AJB8NV4V7YMH', image: '/meta.png' },
-        { title: 'Google Project Management', description: 'In progress...', link: '', image: '/google.png' },
-        { title: 'Certified Associate in Project Management', description: 'In progress...', link: '', image: '/pmi.svg' }
+        { title: 'Google Project Management', description: 'In progress...', link: 54, image: '/google.png' },
+        { title: 'Duolingo', description: 'English Test', link: 'https://certs.duolingo.com/132d6d6d394a5f1893e5eb75578892d8', image: '/duolingo.png' },
+        { title: 'Certified Associate in Project Management', description: 'Future Goal', link: 0, image: '/pmi.svg' }
     ]
 
     return (<>
@@ -26,21 +27,22 @@ export const Certifications = () => {
                         height={80}
                         className={'w-24'}
                     />
-                    <div className={'text-center'}>
+                    <div className={'text-center flex flex-col items-center'}>
                         <h4 className={'text-sm mb-1'}>{item.title}</h4>
                         <p className={'text-muted-foreground text-xs mb-2'}>{item.description}</p>
-                        {item.link ?
+                        {typeof item.link === 'string' ?
                             <Button size={'sm'} variant={'ghost'} asChild>
                                 <Link href={item.link} target={'_blank'}>
                                     Certification
                                     <ExternalLink />
                                 </Link>
                             </Button> :
-
-                            <Button size={'sm'} variant={'ghost'}>
-                                Pending
-                                <FileQuestionMark />
-                            </Button>}
+                            <div>
+                                <div className={'h-2 w-[120px] bg-secondary rounded-lg'}>
+                                    <div className={'h-2 bg-primary rounded-lg'} style={{width: `${120 / 100 * item.link}px`}} />
+                                </div>
+                                <p className={'text-muted-foreground mt-1 text-xs'}>{item.link}%</p>
+                            </div>}
                     </div>
                 </article>
             ))}
